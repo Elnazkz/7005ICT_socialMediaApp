4afd7f14a89cac6000a3e826ecbeac6235582e40
Elnaz <kz_elnaz@yahoo.com>
2023-08-31 11:44:16 +0330

    post_details.blade.php and home_html.css changed to implement new layout

--- resources/views/post_details.blade.php
+++ resources/views/post_details.blade.php
@@ -10,62 +10,95 @@
 
 @section('body')
 
-    <div class="top-div">
-        <h2>Post Details</h2><span>Post Id = {{ $post_id }}</span>
-        <div class="post-list">
+    <div>
+        <h2 class="inline-elem">Post Details</h2>
+        <span class="prepend-20px">Post Id = {{ $post_id }}</span>
+        <div>
             <div class="post-box">
-                <div class="post-box-title">
-                    <h2 class="post-title">Post Title 1</h2>
-                    <input type="button" value="Reply"/>
+                <div>
+                    <span><h2>Post Title 1</h2></span>
+                    <span>No of comments: 2</span>
+                    <span><input type="button" value="Reply"/></span>
                 </div>
-                <p class="post-date">Posted on: 2023/08/29</p>
-                <p class="post-author">Author: Author 1</p>
-                <p class="post-content">some text as the post message 1</p>
+                <p>Posted on: 2023/08/29</p>
+                <p>Author: Author 1</p>
+                <p>some text as the post message 1</p>
             </div>
-            <div class="comment-list">
-                <section class="comment-box accordion">
-                    <input type="checkbox" name="collapse1" id="handle1" checked="checked">
-                    <h2 class="handle comment-title"><label for="handle1"> Comment1 on post 1</label></h2>
-                    <input type="button" value="Reply"/>
-                    <div class="content">
-                        <p>Commented on: 2023/08/29</p>
-                        <p>Author: Author 1</p>
-                        <p>some text as the post message 1</p>
-                    </div>
 
-                    <section class="comment-box accordion">
-                        <input type="checkbox" name="collapse4" id="handle4">
-                        <h2 class="handle comment-title"><label for="handle4"> sub Comment1 for comment 1</label></h2>
+            <div class="comment-box">
+                <details>
+                    <summary role="button" aria-expanded="false" tabindex="0">
+                        <span>No of Comments: 2</span>
                         <input type="button" value="Reply"/>
-                        <div class="content">
-                            <p>Commented on: 2023/08/29</p>
-                            <p>Author: Author 10</p>
-                            <p>some text as the post sub comment 1</p>
-                        </div>
-                    </section>
+                    </summary>
+                    <p>Commented on: 2023/08/29</p>
+                    <p>Author: Author 2</p>
+                    <p>some text as the post message 2</p>
 
-                </section>
-                <section class="comment-box accordion">
-                    <input type="checkbox" name="collapse2" id="handle2">
-                    <h2 class="handle comment-title"><label for="handle2"> Comment2 on post 1</label></h2>
-                    <input type="button" value="Reply"/>
-                    <div class="content">
-                        <p>Commented on: 2023/08/29</p>
-                        <p>Author: Author 2</p>
-                        <p>some text as the post message 2</p>
+                    <div class="comment-box">
+                        <details>
+                            <summary role="button" aria-expanded="false" tabindex="0">
+                                <span>No of Comments: 2</span>
+                                <input type="button" value="Reply"/>
+                            </summary>
+                            <p>Commented on: 2023/08/29</p>
+                            <p>Author: Author 2</p>
+                            <p>some text as the post message 2</p>
+
+                            <div class="comment-box">
+                                <details>
+                                    <summary role="button" aria-expanded="false" tabindex="0">
+                                        <span>No of Comments: 0</span>
+                                        <input type="button" value="Reply"/>
+                                    </summary>
+                                    <p>Commented on: 2023/08/29</p>
+                                    <p>Author: Author 2</p>
+                                    <p>some text as the post message 2</p>
+                                </details>
+                            </div>
+
+                            <div class="comment-box">
+                                <details>
+                                    <summary role="button" aria-expanded="false" tabindex="0">
+                                        <span>No of Comments: 0</span>
+                                        <input type="button" value="Reply"/>
+                                    </summary>
+                                    <p>Commented on: 2023/08/29</p>
+                                    <p>Author: Author 2</p>
+                                    <p>some text as the post message 2</p>
+                                </details>
+                            </div>
+
+                        </details>
                     </div>
-                </section>
-                <section class="comment-box accordion">
-                    <input type="checkbox" name="collapse3" id="handle3">
-                    <h2 class="handle comment-title"><label for="handle3">Comment3 on post 1</label></h2>
-                    <input type="button" value="Reply"/>
-                    <div class="content">
-                        <p>Commented on: 2023/08/29</p>
-                        <p>Author: Author 3</p>
-                        <p>some text as the post message 3</p>
+
+                    <div class="comment-box">
+                        <details>
+                            <summary role="button" aria-expanded="false" tabindex="0">
+                                <span>No of Comments: 0</span>
+                                <input type="button" value="Reply"/>
+                            </summary>
+                            <p>Commented on: 2023/08/29</p>
+                            <p>Author: Author 2</p>
+                            <p>some text as the post message 2</p>
+                        </details>
                     </div>
-                </section>
+
+                </details>
             </div>
+
+            <div class="comment-box">
+                <details>
+                    <summary role="button" aria-expanded="false" tabindex="0" >
+                        <span>No of Comments: 0</span>
+                        <input type="button" value="Reply">
+                    </summary>
+                    <p>Commented on: 2023/08/29</p>
+                    <p>Author: Author 2</p>
+                    <p>some text as the post message 2</p>
+                </details>
+            </div>
+
         </div>
     </div>
 @endsection
